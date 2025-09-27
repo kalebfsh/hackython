f1819d32670b32382496db92b688febed3bc3ce1
kaleb <kalebcolon@ufl.edu>
2025-09-27 14:34:35 -0400

    Chart and log update automatically but dont fully work

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from .models import Pet, MoodEntry
 from django.http import JsonResponse
 from django.contrib.auth import login
 from django.views.decorators.http import require_POST
+import json
+
 
 def signup(request):
     if request.method == 'POST':
@@ -19,13 +21,25 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
+
 @login_required
 def dashboard(request):
     pet, _ = Pet.objects.get_or_create(user=request.user)
     form = MoodForm()
     # load last 14 mood entries
     recent = request.user.moods.order_by('-created_at')[:14]
-    return render(request, 'dashboard.html', {'pet': pet, 'form': form, 'recent': recent})
+
+    moods = MoodEntry.objects.filter(user=request.user).order_by("created_at")
+    mood_dates = [m.created_at.strftime("%Y-%m-%d") for m in moods]
+    mood_values = [m.value for m in moods]
+
+    context = {
+        'pet': pet, 'form': form, 'recent': recent,
+        "mood_dates": json.dumps(mood_dates),
+        "mood_values": json.dumps(mood_values),
+    }
+    return render(request, "dashboard.html", context)
+
 
 @login_required
 @require_POST
@@ -36,12 +50,20 @@ def submit_mood(request):
         mood.user = request.user
         mood.save()
         pet = request.user.pet
+
+        #get last 14 moods
+        moods = MoodEntry.objects.filter(user=request.user).order_by('-created_at')[:14]
+        mood_dates = [m.created_at.strftime("%Y-%m-%d") for m in reversed(moods)]
+        mood_values = [m.value for m in reversed(moods)]
         data = {
             'success': True,
             'pet': pet.as_dict(),
+            'mood_dates': mood_dates,
+            'mood_values': mood_values,
         }
         return JsonResponse(data)
     return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+
 
 @login_required
 def pet_json(request):
